be1ad4abe3a2a2825f31fda43ba447fe764cd904
Eron Lloyd <eronlloyd@protonmail.com>
2022-10-25 19:18:10 -0400

    Initial script ready for testing

--- netbox_optical_scripts.py
+++ netbox_optical_scripts.py
@@ -29,7 +29,7 @@ class CreateCustomFieldsScript(Script):
             label='Attenuation Coefficient',
             group_name='Optical Loss',
             type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The overall optical loss on the cable's fiber"
+            description="The overall optical loss on the fiber across the cable's length"
         )
         attenuation_coeff.content_types.set([cable_id])
         self.log_info("attenuation_coeff custom field created")
@@ -46,10 +46,10 @@ class CreateCustomFieldsScript(Script):
 
         insertion_loss = CustomField(
             name='insertion_loss',
-            label='Attenuator Loss',
+            label='Insertion Loss',
             group_name='Optical Loss',
             type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
+            description="The optical loss between two points in an optical link when passing through a termination in a device"
         )
         insertion_loss.content_types.set([frontport_id, rearport_id])
         self.log_info("insertion_loss custom field created")
@@ -59,7 +59,7 @@ class CreateCustomFieldsScript(Script):
             label='Return Loss',
             group_name='Optical Loss',
             type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
+            description="The optical loss from reflection at a termination point in a device"
         )
         return_loss.content_types.set([frontport_id, rearport_id])
         self.log_info("return_loss custom field created")
@@ -67,19 +67,19 @@ class CreateCustomFieldsScript(Script):
         max_tx_power = CustomField(
             name='max_tx_power',
             label='Maximum TX Power',
-            group_name='Optical Loss',
+            group_name='Optical Power',
             type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
+            description="The maximum transmission power of a transceiver"
         )
-        max_tx_power.content_types.set([frontport_id, rearport_id])
+        max_tx_power.content_types.set([interface_id])
         self.log_info("max_tx_power custom field created")
 
         min_tx_power = CustomField(
             name='min_tx_power',
             label='Minimum TX Power',
-            group_name='Optical Loss',
+            group_name='Optical Power',
             type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
+            description="The minimum transmission power of a transceiver"
         )
         min_tx_power.content_types.set([interface_id])
         self.log_info("min_tx_power custom field created")
@@ -87,9 +87,9 @@ class CreateCustomFieldsScript(Script):
         rx_overload = CustomField(
             name='rx_overload',
             label='RX Overload',
-            group_name='Optical Loss',
+            group_name='Optical Power',
             type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
+            description="The overload power threshold of a transceiver"
         )
         rx_overload.content_types.set([interface_id])
         self.log_info("rx_overload custom field created")
@@ -97,32 +97,33 @@ class CreateCustomFieldsScript(Script):
         rx_sensitivity = CustomField(
             name='rx_sensitivity',
             label='RX Sensitivity',
-            group_name='Optical Loss',
+            group_name='Optical Power',
             type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
+            description="The sensitivity power threshold of a transceiver"
         )
         rx_sensitivity.content_types.set([interface_id])
         self.log_info("rx_sensitivity custom field created")
 
-        rx_wavelength = CustomField(
-            name='rx_wavelength',
-            label='Attenuator Loss',
-            group_name='Optical Loss',
-            type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
-        )
-        rx_wavelength.content_types.set([interface_id])
-        self.log_info("rx_wavelength custom field created")
-
         tx_wavelength = CustomField(
             name='tx_wavelength',
-            label='Attenuator Loss',
-            group_name='Optical Loss',
-            type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
-            description="The intentionally-added loss on the optical link"
+            label='TX Wavelength',
+            group_name='Optical Power',
+            type=CustomFieldTypeChoices.TYPE_TEXT,
+            # This will change in Netbox 3.4 to Decimal
+            description="The specific TX wavelength of the transceiver"
         )
         tx_wavelength.content_types.set([interface_id])
         self.log_info("tx_wavelength custom field created")
 
+        rx_wavelength = CustomField(
+            name='rx_wavelength',
+            label='RX Wavelength',
+            group_name='Optical Power',
+            type=CustomFieldTypeChoices.TYPE_TEXT,  # This will change in Netbox 3.4 to Decimal
+            description="The specific RX wavelength of the transceiver"
+        )
+        rx_wavelength.content_types.set([interface_id])
+        self.log_info("rx_wavelength custom field created")
+
         # Return the output to the script page
-        return attenuation_coeff
+        return 'Generation of custom fields complete.'
